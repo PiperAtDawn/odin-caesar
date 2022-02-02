e7f7a4261f8763b4b6c35055f03cce1bf9ba5715
PiperAtDawn <master_zayats@mail.ru>
2022-02-02 14:48:28 +0300

    Add caesar cipher

--- caesar.rb
+++ caesar.rb
@@ -1,1 +1,20 @@
-
+def caesar_cipher (string, shift)
+  if !shift.between?(-26, 26)
+    adjusted_shift = shift.remainder(26)
+  else
+    adjusted_shift = shift
+  end
+  lowercase = ("a".."z").to_a
+  uppercase = ("A".."Z").to_a
+  result = ""
+  string.split("").each do |c|
+    if lowercase.include?(c)
+      result << lowercase[(lowercase.index(c) + shift) % 26]
+    elsif uppercase.include?(c)
+      result << uppercase[(uppercase.index(c) + shift) % 26]
+    else
+      result << c
+    end
+  end
+  result
+end
